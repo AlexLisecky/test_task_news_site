7d727162c56186777c492ac9c8d4b7c7b5074797
alex <alexlisecky@gmail.com>
2022-01-26 18:33:09 +0300

    add docs for views

--- test_task_news/news_main/views.py
+++ test_task_news/news_main/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, CreateView, UpdateView
 
-from .models import News, Comment
+from .models import News
 
 
 class IndexView(ListView):
+    """ Главная страница"""
     template_name = 'index.html'
     model = News
     queryset = News.objects.all().order_by('-views')[:5]
@@ -13,6 +14,7 @@ class IndexView(ListView):
 
 
 class NewsView(ListView):
+    """ Страница новостей"""
     template_name = 'news_main/news.html'
     model = News
     queryset = News.objects.all()
@@ -20,21 +22,25 @@ class NewsView(ListView):
 
 
 class ContactView(ListView):
+    """ Страница контактов """
     template_name = 'news_main/contacts.html'
     queryset = []
 
 
 class AboutView(ListView):
+    """ Страница о нас """
     template_name = 'news_main/about.html'
     queryset = []
 
 
 class FeedbackView(ListView):
+    """ Страница обратной связи """
     template_name = 'news_main/feedback.html'
     queryset = []
 
 
 class NewsCreateView(CreateView):
+    """ Создание новой новости """
     model = News
     template_name = 'news_main/add_news.html'
     success_url = '/'
@@ -42,6 +48,7 @@ class NewsCreateView(CreateView):
 
 
 class NewsUpdateView(UpdateView):
+    """ Редактирование новости """
     model = News
     template_name = 'news_main/update_news.html'
     success_url = '/'
@@ -49,6 +56,7 @@ class NewsUpdateView(UpdateView):
 
 
 def delete_view(request, pk):
+    """ Удаление новости """
     context = {}
     obj = get_object_or_404(News, pk=pk)
     if request.method == "POST":
